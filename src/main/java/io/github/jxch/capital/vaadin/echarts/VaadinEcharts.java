9ec47205cb5a2150d029c3821e1d08c6d3d9d89e
jxch <qianbuhan@proton.me>
2024-06-02 12:02:39 +0800

    3.2.5-alpha.1.5.2 兼容非标准的json格式

--- src/main/java/io/github/jxch/capital/vaadin/echarts/VaadinEcharts.java
+++ src/main/java/io/github/jxch/capital/vaadin/echarts/VaadinEcharts.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @Tag("vaadin-echarts")
 public class VaadinEcharts extends Div {
     private static String ECHARTS_CDN = null;
-    private String id = UUID.randomUUID().toString();
+    private final String id = UUID.randomUUID().toString();
 
     public VaadinEcharts() {
         this(800, 600);
@@ -30,73 +30,79 @@ public class VaadinEcharts extends Div {
         }
     }
 
-    public void setOption(String jsonOption) {
-        setOption(Json.parse(jsonOption));
+    public void setOption(String standardJson) {
+        setOption(Json.parse(standardJson));
+    }
+
+    public void setOptionNeedConvert(String jsonOption) {
+        String intermediateJson = jsonOption.replaceAll("'", "\"");
+        String standardJson = intermediateJson.replaceAll("(\\w+)(\\s*:\\s*)", "\"$1\"$2");
+        setOption(Json.parse(standardJson));
     }
 
     public void setOption(JsonObject option) {
         getElement().executeJs(String.format("""
-        if (!window.chartInstances) {
-            window.chartInstances = {};
-        }
-        
-        function initializeChart(chartId, chartOption) {
-            var chart = window.chartInstances[chartId];
-            if (chart) {
-                chart.setOption(chartOption);
-            } else {
-                var chartElement = document.getElementById(chartId);
-                if (chartElement) {
-                    chart = echarts.init(chartElement);
-                    chart.setOption(chartOption);
-                    window.chartInstances[chartId] = chart;
+                if (!window.chartInstances) {
+                    window.chartInstances = {};
                 }
-            }
-        }
-        
-        if (window.echarts) {
-            initializeChart('%1$s', %2$s);
-        } else if (!document.querySelector('script[src="%3$s"]')) {
-            var script = document.createElement('script');
-            script.src = '%3$s';
-
-            script.onload = function() {
-                // 在加载ECharts后立即初始化当前图表
-                initializeChart('%1$s', %2$s);
-
-                // 初始化其他任何在脚本加载时尚未初始化的图表
-                for (var chartId in window.chartInstances) {
-                    if (!window.chartInstances[chartId]) {
-                        initializeChart(chartId, window.chartInstances[chartId]);
+                
+                function initializeChart(chartId, chartOption) {
+                    var chart = window.chartInstances[chartId];
+                    if (chart) {
+                        chart.setOption(chartOption);
+                    } else {
+                        var chartElement = document.getElementById(chartId);
+                        if (chartElement) {
+                            chart = echarts.init(chartElement);
+                            chart.setOption(chartOption);
+                            window.chartInstances[chartId] = chart;
+                        }
                     }
                 }
-            };
+                
+                if (window.echarts) {
+                    initializeChart('%1$s', %2$s);
+                } else if (!document.querySelector('script[src="%3$s"]')) {
+                    var script = document.createElement('script');
+                    script.src = '%3$s';
 
-            script.onerror = function() {
-                console.error('ECharts script could not be loaded.');
-            };
+                    script.onload = function() {
+                        // 在加载ECharts后立即初始化当前图表
+                        initializeChart('%1$s', %2$s);
 
-            document.body.appendChild(script);
-        } else {
-            // if the script is already there but not loaded yet,
-            // we can use event listeners to initialize the chart
-            document.querySelector('script[src="%3$s"]').addEventListener('load', function() {
-                initializeChart('%1$s', %2$s);
-            });
-        }
-        """, id, option, ECHARTS_CDN));
+                        // 初始化其他任何在脚本加载时尚未初始化的图表
+                        for (var chartId in window.chartInstances) {
+                            if (!window.chartInstances[chartId]) {
+                                initializeChart(chartId, window.chartInstances[chartId]);
+                            }
+                        }
+                    };
+
+                    script.onerror = function() {
+                        console.error('ECharts script could not be loaded.');
+                    };
+
+                    document.body.appendChild(script);
+                } else {
+                    // if the script is already there but not loaded yet,
+                    // we can use event listeners to initialize the chart
+                    document.querySelector('script[src="%3$s"]').addEventListener('load', function() {
+                        initializeChart('%1$s', %2$s);
+                    });
+                }
+                """, id, option, ECHARTS_CDN));
     }
 
     @Override
     protected void onDetach(DetachEvent detachEvent) {
         super.onDetach(detachEvent);
         getElement().executeJs("""
-            var chart = window.chartInstances['%1$s'];
-            if (chart) {
-                chart.dispose();
-                window.chartInstances['%1$s'] = undefined;
-            }
-        """, id);
+                    var chart = window.chartInstances['%1$s'];
+                    if (chart) {
+                        chart.dispose();
+                        window.chartInstances['%1$s'] = undefined;
+                    }
+                """, id);
     }
 
 }
